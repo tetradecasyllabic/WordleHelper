f2bd8c3b5fc094f7458d89c3877dc6113fad7770
tetradecasyllabic <aiden.h.halper@gmail.com>
2025-09-02 15:19:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const RAW_URL = "https://gist.githubusercontent.com/dracos/dd0668f281e685bad51479e5acaadb93/raw/6bfa15d263d6d5b63840a8e5b64e04b382fdb079/valid-wordle-words.txt";
+const RAW_URL = "words.txt";
 
 let allWords = [];
 let possibleWords = [];
